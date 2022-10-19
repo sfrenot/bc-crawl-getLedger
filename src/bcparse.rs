a84357c168c61398d490a4541f0151898c59dead
Hugo THOMAS <thomas.hugo18@sfr.fr>
2022-10-19 11:05:57 +0100

    linter

--- src/bcparse.rs
+++ src/bcparse.rs
@@ -1,9 +1,11 @@
 use std::fmt;
+
 use bitcoin_hashes::{Hash, sha256d};
+use byteorder::{LittleEndian, ReadBytesExt};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use serde::de::{Error, Visitor};
+
 use crate::bcutils::{get_compact_int, reverse_hash};
-use byteorder::{ReadBytesExt, LittleEndian};
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Block {
@@ -17,7 +19,7 @@ pub struct Block {
     pub timestamp: u32,
     pub bits: u32,
     pub nonce: u32,
-    pub txns: Vec<Transaction>
+    pub txns: Vec<Transaction>,
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
@@ -29,32 +31,32 @@ pub struct Transaction {
     pub inputs: Vec<TxInput>,
     pub outputs: Vec<TxOutput>,
     pub witnesses: Vec<Vec<WitnessItem>>,
-    pub lock_time: u32
+    pub lock_time: u32,
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct TxInput {
     pub prev_output: OutPoint,
     pub signature_script: String,
-    pub sequence: u32
+    pub sequence: u32,
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct OutPoint {
     #[serde(serialize_with = "serialize_hash", deserialize_with = "deserialize_hash")]
     pub hash: String,
-    pub idx: u32
+    pub idx: u32,
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct TxOutput {
     pub value: i64,
-    pub pub_key_script: String
+    pub pub_key_script: String,
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct WitnessItem {
-    pub script: String
+    pub script: String,
 }
 
 #[derive(Debug)]
@@ -66,6 +68,7 @@ fn serialize_hash<S>(hash: &str, s: S) -> Result<S::Ok, S::Error> where S: Seria
 }
 
 struct HashVisitor;
+
 impl<'de> Visitor<'de> for HashVisitor {
     type Value = String;
 
@@ -84,30 +87,30 @@ fn deserialize_hash<'de, D>(d: D) -> Result<String, D::Error> where D: Deseriali
 }
 
 struct Payload<'a> {
-    pl: &'a[u8],
-    off: usize
+    pl: &'a [u8],
+    off: usize,
 }
 
 impl Payload<'_> {
     fn read_u32(&mut self) -> Result<u32, ParsingError> {
-        self.off+=4;
-        Ok(self.pl.get(self.off-4..self.off).ok_or(ParsingError)?.read_u32::<LittleEndian>().unwrap())
+        self.off += 4;
+        Ok(self.pl.get(self.off - 4..self.off).ok_or(ParsingError)?.read_u32::<LittleEndian>().unwrap())
     }
     fn read_i32(&mut self) -> Result<i32, ParsingError> {
-        self.off+=4;
-        Ok(self.pl.get(self.off-4..self.off).ok_or(ParsingError)?.read_i32::<LittleEndian>().unwrap())
+        self.off += 4;
+        Ok(self.pl.get(self.off - 4..self.off).ok_or(ParsingError)?.read_i32::<LittleEndian>().unwrap())
     }
     fn read_i64(&mut self) -> Result<i64, ParsingError> {
-        self.off+=8;
-        Ok(self.pl.get(self.off-8..self.off).ok_or(ParsingError)?.read_i64::<LittleEndian>().unwrap())
+        self.off += 8;
+        Ok(self.pl.get(self.off - 8..self.off).ok_or(ParsingError)?.read_i64::<LittleEndian>().unwrap())
     }
     fn encode_addr(&mut self) -> Result<String, ParsingError> {
-        self.off+=32;
-        Ok(hex::encode(self.pl.get(self.off-32..self.off).ok_or(ParsingError)?))
+        self.off += 32;
+        Ok(hex::encode(self.pl.get(self.off - 32..self.off).ok_or(ParsingError)?))
     }
     fn encode_string(&mut self, length: usize) -> Result<String, ParsingError> {
-        self.off+=length;
-        Ok(hex::encode(self.pl.get(self.off-length..self.off).ok_or(ParsingError)?))
+        self.off += length;
+        Ok(hex::encode(self.pl.get(self.off - length..self.off).ok_or(ParsingError)?))
     }
     fn get_compact_int(&mut self) -> Result<usize, ParsingError> {
         let (txn_count, off) = get_compact_int(&self.pl.get(self.off..).ok_or(ParsingError)?);
@@ -125,22 +128,22 @@ fn tx_hash(tx: &Payload, from: usize) -> String {
 }
 
 fn segwit_hash(tx: &Payload, from: usize, txs_offset: usize) -> String {
-    let tmp = &[&tx.pl[from..from+4],
-        &tx.pl[from+6..txs_offset],
-        &tx.pl[tx.off-4..tx.off]].concat();
+    let tmp = &[&tx.pl[from..from + 4],
+        &tx.pl[from + 6..txs_offset],
+        &tx.pl[tx.off - 4..tx.off]].concat();
 
     hex::encode(sha256d::Hash::hash(tmp))
 }
 
 fn is_segwit(tx: &Payload) -> Result<bool, ParsingError> {
-    Ok(tx.pl.get(tx.off+4..tx.off+6).ok_or(ParsingError)? == &[0x00, 0x01])
+    Ok(tx.pl.get(tx.off + 4..tx.off + 6).ok_or(ParsingError)? == &[0x00, 0x01])
 }
 
 fn tx_loop(pl: &mut Payload, tx_count: usize) -> Result<Vec<Transaction>, ParsingError> {
     let mut txs = Vec::new();
     for _ in 0..tx_count {
         let tx = match is_segwit(pl)? {
-            true =>  parse_segwit_tx(pl)?,
+            true => parse_segwit_tx(pl)?,
             false => parse_standard_tx(pl)?
         };
         txs.push(tx);
@@ -183,7 +186,7 @@ fn parse_segwit_tx(payload: &mut Payload) -> Result<Transaction, ParsingError> {
     let len_in: usize;
     let start = payload.off;
 
-    Ok(Transaction{
+    Ok(Transaction {
         is_segwit: true,
         version: payload.read_i32()?,
         inputs: {
@@ -201,14 +204,14 @@ fn parse_segwit_tx(payload: &mut Payload) -> Result<Transaction, ParsingError> {
         },
         witnesses: witness_loop(payload, len_in)?,
         lock_time: payload.read_u32()?,
-        hash: segwit_hash(payload, start, offset_in_out)
+        hash: segwit_hash(payload, start, offset_in_out),
     })
 }
 
 fn parse_standard_tx(payload: &mut Payload) -> Result<Transaction, ParsingError> {
     let from = payload.off;
 
-    Ok(Transaction{
+    Ok(Transaction {
         is_segwit: false,
         version: payload.read_i32()?,
         inputs: {
@@ -221,7 +224,7 @@ fn parse_standard_tx(payload: &mut Payload) -> Result<Transaction, ParsingError>
         },
         witnesses: vec!(),
         lock_time: payload.read_u32()?,
-        hash: tx_hash(payload, from)
+        hash: tx_hash(payload, from),
     })
 }
 
@@ -229,36 +232,36 @@ fn parse_tx_input(tx_input: &mut Payload) -> Result<TxInput, ParsingError> {
     Ok(TxInput {
         prev_output: OutPoint {
             hash: tx_input.encode_addr()?,
-            idx: tx_input.read_u32()?
+            idx: tx_input.read_u32()?,
         },
         signature_script: {
             let script_length = tx_input.get_compact_int()?;
             tx_input.encode_string(script_length)?
         },
-        sequence: tx_input.read_u32()?
+        sequence: tx_input.read_u32()?,
     })
 }
 
 fn parse_tx_output(tx_output: &mut Payload) -> Result<TxOutput, ParsingError> {
-    Ok(TxOutput{
+    Ok(TxOutput {
         value: tx_output.read_i64()?,
         pub_key_script: {
             let script_length = tx_output.get_compact_int()?;
             tx_output.encode_string(script_length)?
-        }
+        },
     })
 }
 
 fn parse_witness_item(tx_witness: &mut Payload) -> Result<WitnessItem, ParsingError> {
     let length = tx_witness.get_compact_int()?;
-    Ok(WitnessItem{
+    Ok(WitnessItem {
         script: tx_witness.encode_string(length)?
     })
 }
 
 //Public Entry
 pub fn parse_block(payload: &[u8]) -> Result<Block, ParsingError> {
-    let mut block = Payload{ pl: payload, off: 0};
+    let mut block = Payload { pl: payload, off: 0 };
     Ok(Block {
         hash: block_hash(&block)?,
         version: block.read_i32()?,
@@ -270,6 +273,6 @@ pub fn parse_block(payload: &[u8]) -> Result<Block, ParsingError> {
         txns: {
             let tx_count = block.get_compact_int()?;
             tx_loop(&mut block, tx_count)?
-        }
+        },
     })
 }
